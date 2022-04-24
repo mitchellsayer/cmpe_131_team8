bc45bd740469e4a1982a7781e2eecb7d7cc81e89
Muhammad Fawad Aleem <muhammad.f.aleem@gmail.com>
2022-04-23 23:08:26 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,20 @@
 from flask import Flask
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
 
 myapp = Flask(__name__)
 
-from app import routes
+myapp.config.from_mapping(
+	SECRET_KEY = "PICK ONE",
+	SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(base_dir,
+					 "app.db")
+	)
+
+db = SQLAlchemy(myapp)
+login = LoginManager(myapp)
+login.login_view = "Login"
+
+from app import route
